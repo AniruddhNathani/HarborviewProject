67ee2b05df33205fc17ff99157b416ec473b5b32
Aniruddh Nathani <aniruddh.nathani@sap.com>
2021-05-07 11:51:18 -0700

    Added session object to store responses

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template, request
-from model import load_db, response
+from flask import Flask, render_template, request, session
+from model import load_db
+from flask_session import Session
+
 
 # from HarborviewProject.model import model
 app = Flask(__name__)
+SESSION_TYPE = 'filesystem'
+app.config.from_object(__name__)
+Session(app)
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -13,14 +18,15 @@ def welcome():
 @app.route("/languages", methods=["GET", "POST"])
 def languages():
     db = load_db()
-    response_list = response()
-    return render_template("languages.html", language=db, response_list=response_list)
+    session["response_list"] = []
+    return render_template("languages.html", language=db)
 
 
 @app.route("/lang_index=<int:index>/screening/screening_one", methods=["GET", "POST"])
 def screening_one(index):
     db = load_db()
     language = db[index]
+    session["response_list"].append(["Bye"])
     return render_template("screening_one.html", language=language, index=index)
 
 
@@ -28,5 +34,10 @@ def screening_one(index):
 def screening_two(index):
     db = load_db()
     language = db[index]
+    session["response_list"].append(["Bye 2"])
+    print(session["response_list"])
     return render_template("screening_two.html", language=language)
 
+
+if __name__ == "__main__":
+    app.run()
--- model.py
+++ model.py
@@ -6,9 +6,4 @@ def load_db():
         return json.load(f)
 
 
-def response():
-    response_list = ["Hi"]
-    return response_list
-
-
 db = load_db()
